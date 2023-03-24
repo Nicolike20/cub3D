7f560c5442effc2e2fa6ea25be87fdf3d4358289
nicolike <nicolike3.0@gmail.com>
2023-03-24 15:07:10 +0100

    testing stuff on linux

--- src/parser/textures.c
+++ src/parser/textures.c
@@ -25,6 +25,7 @@ void	load_image(t_mlx *mlx, t_tex *tex, char *path)
 	}
 	printf("path1: %s\nmlx: %p\ntex->tw: %d\ntex->th: %d\n", path, mlx, tex->tw, tex->th);
 	tex->img.img = mlx_xpm_file_to_image(mlx, path, &tex->tw, &tex->th);
+	perror("error:");
 	printf("path2: %s\nmlx: %p\ntex->tw: %d\ntex->th: %d\n", path, mlx, tex->tw, tex->th);
 //	if (!(tex->img.img = mlx_xpm_file_to_image(mlx, path, &tex->tw, &tex->th)))
 	if (tex->img.img == NULL)
@@ -42,8 +43,9 @@ void	load_image(t_mlx *mlx, t_tex *tex, char *path)
 }
 void	loading_textures(t_mlx *mlx, t_map *vars)
 {
-	mlx->tex = (t_tex *)malloc(sizeof(t_tex) * 5);
-//	printf("test vars->no: %s\n", vars->no);
+	mlx->tex = (t_tex *)malloc(sizeof(t_tex) * 5); //proteger este malloc;
+	printf("test tex: %p\n", &mlx->tex[T_NORTH].tw);
+	printf("test tex: %p\n", &mlx->tex[T_NORTH].th);
 	load_image(mlx, &mlx->tex[T_NORTH], vars->no);
 	load_image(mlx, &mlx->tex[T_SOUTH], vars->so);
 	load_image(mlx, &mlx->tex[T_WEST], vars->we);
